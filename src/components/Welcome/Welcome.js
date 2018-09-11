41e42ff57dd7c4aa6dfe2ae270202c80552430c0
Sunny Wong <swong194@gmail.com>
2018-09-11 14:34:17 -0400

    add hello world

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
-class Welcome extends Component {
-  render() {
-    return <h1>Welcome!</h1>
+const styles = {
+  Text: {
+    padding: '20px;'
+  },
+  Paper: {
+    margin: '20px'
   }
 }
 
-export default Welcome;
+class Welcome extends Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Paper elevation={1} square={true} className={classes.Paper}>
+        <Typography variant='display1' align='center' className={classes.Text}>
+          Hello World!
+        </Typography>
+        <Typography variant='subheading' align='justify' className={classes.Text}>
+          Material design has become a leading standard in how apps should look and feel.
+          So I decided to teach myself by making the hello world of apps, the TODO app.
+          This project is created using React, Redux and Material-UI
+        </Typography>
+        <Typography variant='caption' align='right' className={classes.Text}>
+          <Button href="https://github.com/swong194" variant="contained" color="secondary">
+              By Sunny Wong
+          </Button>
+        </Typography>
+      </Paper>
+    );
+  }
+}
+
+export default withStyles(styles)(Welcome);
